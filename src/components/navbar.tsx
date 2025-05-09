fe9c674b77d294f04a90ad83e4e0eff1ab0f0f90
VivekPoongodi <viveksankar2010@gmail.com>
2025-05-09 05:51:25 +0530

    builded

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,7 +18,7 @@ import {
     NavigationMenu,
     NavigationMenuContent,
     NavigationMenuItem,
-    NavigationMenuLink,
+   
     NavigationMenuList,
     NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
